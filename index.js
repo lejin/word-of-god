e3793b33247be9caae902c797419cdca4837b3e8
Lejin Joseph <lejinjoseph@outlook.com>
2020-08-01 16:13:56 +0530

    change for reading quote from json

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  function restore_options() {
+// stored in chrome.storage.
+function restore_options() {
     // Use default value language=english.
     chrome.storage.sync.get({
-      language: 'english'
-      
-    }, function(items) {
-        if(items.language=='malayalam'){
-            document.getElementById('quote').textContent='എന്നെ ശക്‌തനാക്കുന്നവനിലൂടെ എല്ലാം ചെയ്യാന്‍ എനിക്കു സാധിക്കും.';
-            document.getElementById('verse').textContent='ഫിലിപ്പി 4 : 13';
-        }else{
-            document.getElementById('quote').textContent='I Can Do All Things Through Christ Who Strengthens Me.';
-            document.getElementById('verse').textContent='Philippians 4:13';
+        language: 'english'
+
+    }, function (items) {
+        if (items.language == 'malayalam') {
+            const url = chrome.runtime.getURL('malayalam.json');
+
+            fetch(url)
+                .then((response) => response.json())
+                .then((json) => generateRandomQuote(json));
+
+        } else {
+            const url = chrome.runtime.getURL('english.json');
+
+            fetch(url)
+                .then((response) => response.json())
+                .then((json) => generateRandomQuote(json));
         }
-    
+
     });
+}
+
+function generateRandomQuote(json) {
+    var randomNumber=getRandomInt(json.length);
+    document.getElementById('quote').textContent = json[randomNumber].quote;
+    document.getElementById('verse').textContent = json[randomNumber].verse;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
   }
-  
-  document.addEventListener('DOMContentLoaded', restore_options);
+
+document.addEventListener('DOMContentLoaded', restore_options);
