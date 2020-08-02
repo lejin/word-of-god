0606b1bc0d4e71e5eaf20a27f9cbed151b65972f
Dijo Davis <dijodevpd@gmail.com>
2020-08-02 13:37:23 +0530

    icons added + popup design updated

--- js/popup.js
+++ js/popup.js
@@ -2,8 +2,13 @@ function displayPopup() {
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
   } else {
-    window.open(chrome.runtime.getURL('options.html'));
+    window.open(chrome.runtime.getURL('/html/options.html'));
   }
 }
 
+function openContactPage() {
+  chrome.tabs.create({url: "http://jyinfopark.in"});
+}
+
 document.getElementById('go-to-options').addEventListener('click', displayPopup);
+document.getElementById('openContactPage').addEventListener('click', openContactPage);
