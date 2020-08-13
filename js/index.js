a9644be110cefa2a6abef2fc91eccc3afe088041
Dijo Davis <dijodevpd@gmail.com>
2020-08-13 16:10:36 +0530

    Chrome Color JS lib added

--- js/index.js
+++ js/index.js
@@ -1,59 +1,88 @@
-let bgColors = ["#B71C1C", "#880E4F", "#4A148C", "#311b92", "#1a237e", "#0d47a1", "#01579b", "#006064", "#f57f17", "#1b5e20", "#33691E"];
+/**
+ * New Tab Object/Namespace
+ */
+let newTab = {
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function showQuote() {
-    // Use default value language=english.
-    chrome.storage.sync.get({
-        language: 'english'
+    //predefined BG colors
+    bgColors: ["#B71C1C", "#880E4F", "#4A148C", "#311b92", "#1a237e", "#0d47a1", "#01579b", "#006064", "#f57f17", "#1b5e20", "#33691E"],
 
-    }, function (items) {
-        if (items.language == 'malayalam') {
-            const url = chrome.runtime.getURL('/data/malayalam.json');
+    //Generate random integer
+    getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    },
 
-            fetch(url)
-                .then((response) => response.json())
-                .then((json) => generateRandomQuote(json));
+    getRandomColor() {
+        var randomNumber = newTab.getRandomInt(newTab.bgColors.length);
+        return newTab.bgColors[randomNumber];
+    },
 
-        } else {
-            const url = chrome.runtime.getURL('/data/english.json');
+    //set random BG from the predefiend list above and saves to prevent repetition
+    setRandomBGColor() {
+        var bgColor = newTab.getRandomColor();
+        chrome.storage.local.get(['lastColor'], function (result) {
+            if (null != result.lastColor && result.lastColor == bgColor) {
+                do {
+                    bgColor = newTab.getRandomColor();
+                } while (bgColor != result.lastColor);
+            }
+        });
+        document.body.style.backgroundColor = bgColor;
+        document.body.style.color = "#f1efe6";
+        chrome.storage.local.set({ lastColor: bgColor }, function () {
+        });
+    },
 
-            fetch(url)
-                .then((response) => response.json())
-                .then((json) => generateRandomQuote(json));
+    //get random color using Chroma.js
+    //https://gka.github.io/chroma.js/#chroma-random
+    chromaSetRandomBG() {
+        let maxSaturation = 0.5;
+        let maxLightness = 0.4;
+        let randomColor = chroma.random();
+
+        if (randomColor.get("hsl.s") > maxSaturation) {
+            randomColor = randomColor.set("hsl.s", maxSaturation);
         }
 
-    });
-}
-
-function generateRandomQuote(json) {
-    setRandomBGColor();
-    var randomNumber = getRandomInt(json.length);
-    document.getElementById('quote').textContent = json[randomNumber].quote;
-    document.getElementById('verse').textContent = json[randomNumber].verse;
-}
-
-function setRandomBGColor() {
-    var bgColor=getRandomColor();
-    chrome.storage.local.get(['lastColor'], function(result) {
-        if(null!=result.lastColor && result.lastColor==bgColor){
-            do{
-               bgColor=getRandomColor(); 
-            }while(bgColor!=result.lastColor);
+        if (randomColor.get("hsl.l") > maxLightness) {
+            randomColor = randomColor.set("hsl.l", maxLightness);
         }
-      });
-    document.body.style.backgroundColor = bgColor;
-    chrome.storage.local.set({lastColor: bgColor}, function() {
-      });
-}
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
+        document.body.style.color = "#fff";
+        document.body.style.backgroundColor = randomColor;
+    },
 
-document.addEventListener('DOMContentLoaded', showQuote);
 
-function getRandomColor(){
-    var randomNumber = getRandomInt(bgColors.length);
-    return bgColors[randomNumber];
-}
+
+    // Restores select box and checkbox state using the preferences
+    // stored in chrome.storage.
+    showQuote() {
+        // Use default value language=english.
+        chrome.storage.sync.get({
+            language: 'english'
+        }, function (items) {
+            if (items.language == 'malayalam') {
+                const url = chrome.runtime.getURL('/data/malayalam.json');
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((json) => newTab.generateRandomQuote(json));
+            } else {
+                const url = chrome.runtime.getURL('/data/english.json');
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((json) => newTab.generateRandomQuote(json));
+            }
+        });
+    },
+
+    //Show random quote on new tab
+    generateRandomQuote(json) {
+        //newTab.setRandomBGColor();
+        newTab.chromaSetRandomBG();
+        var randomNumber = newTab.getRandomInt(json.length);
+        document.getElementById('quote').textContent = json[randomNumber].quote;
+        document.getElementById('verse').textContent = json[randomNumber].verse;
+    },
+
+};
+
+document.addEventListener('DOMContentLoaded', newTab.showQuote);
