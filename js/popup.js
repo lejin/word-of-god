dd3e23798c9006b949bbc1be46c1b4c39e75d0c6
Dijo Davis <dijodevpd@gmail.com>
2020-08-13 10:56:26 +0530

    Popup code optimization + version number dynamic

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,38 @@
-function displayPopup() {
-  if (chrome.runtime.openOptionsPage) {
-    chrome.runtime.openOptionsPage();
-  } else {
-    window.open(chrome.runtime.getURL('/html/options.html'));
+/**
+ * Popup Object/Namespace
+ */
+let popup = {
+
+  //Initialize - add events and necessary contents on DOM load
+  init() {
+    document.getElementById('go-to-options').addEventListener('click', popup.displayPopup);
+    document.getElementById('openContactPage').addEventListener('click', popup.openContactPage);
+    popup.setVersionNumber();
+  },
+
+  //Open options page
+  displayPopup() {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL('/html/options.html'));
+    }
+  },
+
+  //Open contact page link in new tab
+  openContactPage() {
+    chrome.tabs.create({ url: "http://jyinfopark.in" });
+  },
+
+  //read and set version number from Manifest.json
+  setVersionNumber() {
+    var manifestData = chrome.runtime.getManifest();
+    document.getElementById("vNum").innerText = `v${manifestData.version}`;
   }
-}
+};
 
-function openContactPage() {
-  chrome.tabs.create({url: "http://jyinfopark.in"});
-}
+/**
+ * Initialize popup on DOM contents loaded/Popup open
+ */
+document.addEventListener('DOMContentLoaded', popup.init);
 
-document.getElementById('go-to-options').addEventListener('click', displayPopup);
-document.getElementById('openContactPage').addEventListener('click', openContactPage);
