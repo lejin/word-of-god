a70c6ee55b6550b66a10f051004d5a44b9382da6
rincyphilip <rendezrin.012@gmail.com>
2021-03-25 19:26:21 +0400

    search-bar v1

--- js/index.js
+++ js/index.js
@@ -200,3 +200,20 @@ let newTab = {
 };
 
 document.addEventListener('DOMContentLoaded', newTab.showQuote);
+
+
+// search
+
+const f = document.getElementById('search-form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=';
+
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + '+' + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
